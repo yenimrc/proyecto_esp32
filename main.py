15b6f37c7892d2d14e87df50f21097cec4801384
yeni mrc <yennialgc562425@gmail.com>
2026-05-28 22:58:32 -0600

    se añadio la coleccion

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ from datetime import datetime
 
 app = FastAPI()
 
-# conexión MongoDB Atlas
-MONGO_URI = "mongodb+srv://esp32_proyecto:proyecto123@cluster0.kqtluum.mongodb.net/proyecto=Cluster0"
-
+MONGO_URI = "mongodb+srv://esp32_proyecto:proyecto123@cluster0.kqtluum.mongodb.net/?retryWrites=true&w=majority"
 
 client = MongoClient(MONGO_URI)
-db = client.iot
-collection = db.sensores
+
+db = client["proyecto"]     
+collection = db["sensor"]   
 
 @app.get("/")
 def root():
-    return {"mensaje":"API funcionando"}
+    return {"mensaje": "API funcionando"}
 
 @app.post("/sensor")
 def guardar_sensor(data: dict):
+    # Agrega la fecha y hora de registro de forma automática al recibir el JSON
+    data["fecha"] = datetime.now().isoformat()
 
-    data["fecha"] = datetime.now()
-
+    # Guarda el JSON directo en la colección 'sensor'
     collection.insert_one(data)
 
-    return {"status":"dato guardado"}
+    return {"status": "dato guardado"}
